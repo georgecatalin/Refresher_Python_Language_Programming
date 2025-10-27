64e1002893dd15bf667a82f038ce61df30b25377
George Calin <george.calin@gmail.com>
2025-10-27 13:32:17 +0200

    Add initial blog app structure with templates, views, and URL routing

---
+++ Python_Django_the_practical_guide/building_a_blog/my_site/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.start_page, name="start_page"),
+    path("/posts", views.list_posts, name="list_page"),
+    path("/posts/<slug:slug>",views.detail_post, name="detail_page")
+]
--- Python_Django_the_practical_guide/building_a_blog/my_site/blog/views.py
+++ Python_Django_the_practical_guide/building_a_blog/my_site/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def start_page(request):
+    return render(request,"blog/index.html")
+
+
+def list_posts(request):
+    pass
+
+
+def detail_post(request):
+    pass
