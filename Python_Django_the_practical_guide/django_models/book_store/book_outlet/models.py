f19d70d671518ee846dc8eb413e7c383713be3d6
George Calin <george.calin@gmail.com>
2025-10-28 14:43:23 +0200

    Add initial migration and update Book model with author and best seller fields

--- Python_Django_the_practical_guide/django_models/book_store/book_outlet/models.py
+++ Python_Django_the_practical_guide/django_models/book_store/book_outlet/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=60)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(16)])
+    author = models.CharField(null = True, max_length=50)
+    is_best_seller = models.BooleanField(default= False )
 
-    
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
