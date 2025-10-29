13509492352868d61b31e9d4064ce0c0f6a33df2
George Calin <george.calin@gmail.com>
2025-10-29 16:24:18 +0200

    Add base template and implement book listing and detail views

--- Python_Django_the_practical_guide/django_models/book_store/book_outlet/views.py
+++ Python_Django_the_practical_guide/django_models/book_store/book_outlet/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Book
 
 # Create your views here.
+def index(request):
+   books = Book.objects.all()
+   return render(request,"book_outlet/index.html", {"books" : books})
+
+
+def get_detail(request, id):
+   book = get_object_or_404(Book,pk=id)
+   return render(request, "book_outlet/book_detail.html" , {
+      "title": book.title,
+      "rating":book.rating,
+      "author":book.author,
+      "is_best_seller" : book.is_best_seller
+   })
