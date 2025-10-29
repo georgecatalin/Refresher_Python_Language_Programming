d5f6897667a08dadaa789aa2eb3ffa0e33f57efa
George Calin <george.calin@gmail.com>
2025-10-29 16:29:06 +0200

    Add get_absolute_url method to Book model and update index template for URL rendering

--- Python_Django_the_practical_guide/django_models/book_store/book_outlet/models.py
+++ Python_Django_the_practical_guide/django_models/book_store/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
+from django.urls import reverse
 
 # Create your models here.
 class Book(models.Model):
@@ -7,6 +8,11 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(16)])
     author = models.CharField(null = True, max_length=50)
     is_best_seller = models.BooleanField(default= False )
+
+
+    def get_absolute_url(self):
+        return reverse("detail-book", args=[self.id])
+    
 
 
     def __str__(self):
