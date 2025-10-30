0024705a3ab68784dba73f83ccd775d4b866589a
George Calin <george.calin@gmail.com>
2025-10-30 15:50:56 +0200

    Update BookAdmin to include readonly slug field and enhance list display and filtering options

--- Python_Django_the_practical_guide/django_models/book_store/book_outlet/admin.py
+++ Python_Django_the_practical_guide/django_models/book_store/book_outlet/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    readonly_fields = ["slug"]
+    prepopulated_fields = {"slug":["title"]}
+    list_filter = ("author",)
+    list_display = ("title", "rating", "author")
 
 # Register your models here.
 admin.site.register(Book, BookAdmin)
