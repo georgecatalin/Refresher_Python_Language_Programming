3f31348ab0316873c69e56da3bd5ad314000580b
George Calin <george.calin@gmail.com>
2025-10-30 16:14:28 +0200

    add a one-to-one relationship and makemigrations plus migrate

--- Python_Django_the_practical_guide/django_models/book_store/book_outlet/models.py
+++ Python_Django_the_practical_guide/django_models/book_store/book_outlet/models.py
@@ -4,10 +4,18 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+
 class Book(models.Model):
     title = models.CharField(max_length=60)
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(16)])
-    author = models.CharField(null = True, max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_best_seller = models.BooleanField(default= False )
     slug = models.SlugField(default="",null=False, blank=True, db_index=True)
 
