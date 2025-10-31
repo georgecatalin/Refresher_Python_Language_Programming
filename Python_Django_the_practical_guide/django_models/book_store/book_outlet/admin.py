0e0ae431596d504f0a345a81c2083fd893f4242d
George Calin <george.calin@gmail.com>
2025-10-31 13:48:08 +0200

    adding a one-to-one relationship in Django code and Django Admin web section

--- Python_Django_the_practical_guide/django_models/book_store/book_outlet/admin.py
+++ Python_Django_the_practical_guide/django_models/book_store/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book,Author
+from .models import Book,Author,Address
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":["title"]}
@@ -9,3 +9,4 @@ class BookAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
+admin.site.register(Address)
--- Python_Django_the_practical_guide/django_models/book_store/book_outlet/models.py
+++ Python_Django_the_practical_guide/django_models/book_store/book_outlet/models.py
@@ -5,10 +5,23 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=9)
+    city = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "{},{}-{}".format(self.street, self.postal_code,self.city)
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address,on_delete=models.CASCADE,null=True)
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
