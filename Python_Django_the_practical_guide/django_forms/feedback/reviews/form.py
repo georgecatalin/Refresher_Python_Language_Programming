6b897075b63eee99201187896b964498dd073f22
George Calin <george.calin@gmail.com>
2025-11-03 17:35:54 +0200

    customize the rendered HTML by controlling elements of the form

--- Python_Django_the_practical_guide/django_forms/feedback/reviews/form.py
+++ Python_Django_the_practical_guide/django_forms/feedback/reviews/form.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(max_length=10, label="user name", required=True, error_messages={"required": "You have to enter a name", "max_length":"You have exceeded the maximul length permitted"})
+    user_name = forms.CharField(label="user name", max_length=10, error_messages={ 
+        "required": "You have to enter a name", 
+        "max_length":"You have exceeded the maximum length permitted"
+        })
