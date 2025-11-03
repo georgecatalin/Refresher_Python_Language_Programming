c616c33cee6ce7c5c25e17b164f78f2a31e7c2b2
George Calin <george.calin@gmail.com>
2025-11-03 11:54:06 +0200

    Implement models and admin configuration for Post, Author, and Tag; update views and templates for post detail display

--- Python_Django_the_practical_guide/building_a_blog/my_site/blog/models.py
+++ Python_Django_the_practical_guide/building_a_blog/my_site/blog/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 
+
+
+
+
+
+
 # Create your models here.
+class Tag(models.Model):
+    captions = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{}".format(self.captions)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+
+    def get_full_name(self):
+        return "{} {}".format(self.first_name,self.last_name)
+    
+    def __str__(self):
+        return self.get_full_name()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     excerpt = models.CharField(max_length=300)
@@ -9,3 +34,5 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index= True)
     content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author,on_delete=models.SET_NULL, null= True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
