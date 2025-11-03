d7d45f12b6cae13d1604493fbeb77089cc437e06
George Calin <george.calin@gmail.com>
2025-11-03 20:27:01 +0200

    save to database

--- Python_Django_the_practical_guide/django_forms/feedback/reviews/views.py
+++ Python_Django_the_practical_guide/django_forms/feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .form import ReviewForm
+from .models import Review
 
 
 # Create your views here.
@@ -12,6 +13,19 @@ def display(request):
 
       if this_form.is_valid():
          print(this_form.cleaned_data)
+ 
+ 
+         this_data = Review(
+            user_name=this_form.cleaned_data['user_name'],
+            text_review =this_form.cleaned_data['text_review'],
+            rating = this_form.cleaned_data['rating']
+            )
+         
+         this_data.save()
+
+
+
+
          return HttpResponseRedirect(reverse('thank-you-page')) 
       
     else:
