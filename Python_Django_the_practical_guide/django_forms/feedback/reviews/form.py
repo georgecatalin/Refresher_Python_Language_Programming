1ae5762dc9b7d592c55b77656cd298520f5b39c0
George Calin <george.calin@gmail.com>
2025-11-03 19:58:47 +0200

    adding more controls to a form

--- Python_Django_the_practical_guide/django_forms/feedback/reviews/form.py
+++ Python_Django_the_practical_guide/django_forms/feedback/reviews/form.py
@@ -5,3 +5,5 @@ class ReviewForm(forms.Form):
         "required": "You have to enter a name", 
         "max_length":"You have exceeded the maximum length permitted"
         })
+    text_review = forms.CharField(label="Your Review", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=10)
