cc2a1e606b8544eabc9daad6333f5476f6add709
George Calin <george.calin@gmail.com>
2025-11-03 16:18:37 +0200

    create a class and use it to encapsulate a user form

--- Python_Django_the_practical_guide/django_forms/feedback/reviews/views.py
+++ Python_Django_the_practical_guide/django_forms/feedback/reviews/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .form import ReviewForm
 
 
 # Create your views here.
 def display(request):
     if request.method == 'POST':
-        user_name = request.POST['this_user']
-        print(user_name)
-        if user_name == "" or len(user_name) > 100:
-            return render(request, "reviews/review.html", { "has_error": True})
-        return HttpResponseRedirect(reverse('thank-you-page'))
+      
+      this_form = ReviewForm(request.POST)
 
-    return render(request,"reviews/review.html", {"has_error":False})
+      if this_form.is_valid():
+         print(this_form.cleaned_data)
+         return HttpResponseRedirect(reverse('thank-you-page'))
+      
+    this_form = ReviewForm()
+    return render(request,"reviews/review.html", {"form":this_form})
 
 
 def display_thank(request):
