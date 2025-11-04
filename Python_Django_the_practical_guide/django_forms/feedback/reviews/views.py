88d07d8c7a54fe641fcbf27007533f90ad3b7176
George Calin <george.calin@gmail.com>
2025-11-04 11:25:07 +0200

    pass data from a template view with function get_context_data()

--- Python_Django_the_practical_guide/django_forms/feedback/reviews/views.py
+++ Python_Django_the_practical_guide/django_forms/feedback/reviews/views.py
@@ -62,3 +62,8 @@ class ReviewView(View):
 # replace the standard class view with a template view
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "Hello from template view."
+        return context
